ef7e3770477c57223dd1f15c6c9cc450538b6855
Robert Witt <robert.witt@zalando.de>
2018-07-31 20:01:37 +0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Robert Witt on 31.07.18.
+//  Copyright © 2018 Robert Witt. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title = ""
+    var done = false
+    
+}
